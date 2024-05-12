0c99279b88df1b45a37768c92dcc0d2ac5afc037
Diego López Ariza <diegoawesome2017@outlook.com>
2024-05-12 16:58:01 -0600

    added directory selector

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import flet as ft
-import shutil
 from flet import icons
 from yt_dlp import YoutubeDL as YDL
-
 
 def main(page: ft.Page):
     page.title = "Flet counter example"
@@ -23,7 +21,13 @@ def main(page: ft.Page):
     )
     
     link_input = ft.TextField(label="Escribe el link de tu video", width=200)
-
+    
+    
+    def get_directory_path(e):
+        download(download_path=e.path)
+         
+    filepicker = ft.FilePicker(on_result=get_directory_path)
+    
     def progress_hook(d):
         page.add(
             status_bar
@@ -35,48 +39,59 @@ def main(page: ft.Page):
             progress_bar.value = float(d['_percent_str'].replace('%','')) * 0.01
             status_text.value = f"Descargando... {d['_percent_str']} a {d['_speed_str']}"
             page.update()
-        status_text.value = "Descargar otro video"
         page.controls.pop()
+    
+    
+    global added_thumbail    
+    added_thumbail = False
+    
+    def add_thumbnail(thumb, title):
+        global added_thumbail
+        if added_thumbail:
+            page.controls.pop()
+        page.add(
+                    ft.Row(
+                        [
+                            ft.Image(thumb, width=200,border_radius=ft.border_radius.all(10)),
+                            ft.Text(title,width=200,overflow=ft.TextOverflow.ELLIPSIS, max_lines=4)
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                    ),
+                )
+        page.update()
+        if not added_thumbail:
+            added_thumbail = True
             
-            
-    def download_video(url):
-        with YDL() as ydl:
+    def download_video(url,path):
+        with YDL({
+                'outtmpl': fr"{path}\%(title)s.%(ext)s",
+            }) as ydl:
             ydl.add_progress_hook(progress_hook)
-            ydl.build_format_selector(format_spec='1080x1920+bestaudio/best')
-            ydl.format_resolution(format='1080x1920+bestaudio/best')
             info = ydl.extract_info(url,download=False)
             filename = ydl.prepare_filename(info)
-            ydl.download([url])
-            oldpath = ydl.get_output_path()
-            filepicker = ft.FilePicker(dialog_title="Selecciona la carpeta de descarga")
-            result = filepicker.save_file(filename=filename,file_type="MEDIA")
-            newpath = result.path
-            shutil.move(oldpath,newpath)
-            ##page.launch_url(path)
+            filepath = ydl.get_output_path(filename=filename)
+            if ydl._ensure_dir_exists(filepath):
+                status_text.value = "Video ya descargado"
+            ydl.build_format_selector(format_spec='bestvideo+bestaudio/best')
+            ydl.download(url_list=[url])
             thumbails = info.get('thumbnails', [])
             for thumb in thumbails:
                 if 'resolution' not in thumb:
                     continue
                 if thumb['resolution'] != "1920x1080":
                     continue
-                page.add(
-                    ft.Row(
-                        [
-                            ft.Image(thumb['url'], width=200, height=200,border_radius=ft.border_radius.all(10)),
-                            ft.Text(info['title'],width=200,overflow=ft.TextOverflow.ELLIPSIS, max_lines=4)
-                        ],
-                        alignment=ft.MainAxisAlignment.CENTER,
-                    ),
-                )
-                page.update()
+                add_thumbnail(thumb['url'],info['title'])
+                
             
 
-    def download(e):
+    def download(download_path):
         try:
-            download_video(str(link_input.value))
-        except:
+            download_video(str(link_input.value),download_path)
+        except Exception as e:
+            print(e)
             status_text.value = "Link inválido"
             page.update()
+
 
     page.navigation_bar = ft.NavigationBar(
         destinations=[
@@ -87,13 +102,15 @@ def main(page: ft.Page):
             icon=icons.BOOKMARK, label="Saved"
         ),
         ]
-        
     )
+    
+    
+    page.overlay.extend([filepicker])
     page.add(
         ft.Row(
             [
                 link_input,
-                ft.IconButton(icons.CLOUD_DOWNLOAD, on_click=download),
+                ft.IconButton(icons.CLOUD_DOWNLOAD, on_click=lambda _: filepicker.get_directory_path()),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         ),
