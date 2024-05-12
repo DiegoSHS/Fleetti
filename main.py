65d50b660dd246d92a76f66cf4dad6d5c4f54b6c
Diego López Ariza <diegoawesome2017@outlook.com>
2024-05-12 00:33:07 -0600

    Added progress bar and video thumbail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import flet as ft
+import json
 from flet import icons
 from yt_dlp import YoutubeDL as YDL
 
@@ -6,32 +7,67 @@ from yt_dlp import YoutubeDL as YDL
 def main(page: ft.Page):
     page.title = "Flet counter example"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-
-    video_status = ft.TextField(value="Aún no has descargado ningún video",read_only=True,border=ft.InputBorder.NONE)
+    status_text = ft.TextField(value="Aún no has descargado ningún video",read_only=True,border=ft.InputBorder.NONE)
+    progress_bar = ft.ProgressBar(width=400)
+    status_bar = ft.Row(
+                [
+                    progress_bar,
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            )
+    video_status = ft.Row(
+        [
+            status_text,
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+    )
+    
     link_input = ft.TextField(label="Escribe el link de tu video", width=200)
 
     def progress_hook(d):
-        print(d)
+        page.add(
+            status_bar
+        )
         if d['status'] == 'finished':
-            video_status.value = "Video descargado"
+            status_text.value = "Video descargado"
             page.update()
         else:
-            video_status.value = f"Descargando... {d['_percent_str']} a {d['_speed_str']}"
+            progress_bar.value = float(d['_percent_str'].replace('%','')) * 0.01
+            status_text.value = f"Descargando... {d['_percent_str']} a {d['_speed_str']}"
             page.update()
+        status_text.value = "Descargar otro video"
+        page.controls.pop()
             
             
     def download_video(url):
-        dl_opts = {
-            'progress_hooks': [progress_hook],
-        }
-        with YDL(dl_opts) as ydl:
+        with YDL() as ydl:
+            ydl.add_progress_hook(progress_hook)
+            ydl.build_format_selector(format_spec='1080x1920+bestaudio/best')
+            info = ydl.extract_info(url,download=False)
             ydl.download([url])
+            thumbails = info.get('thumbnails', [])
+            for thumb in thumbails:
+                if 'resolution' not in thumb:
+                    continue
+                if thumb['resolution'] != "1920x1080":
+                    continue
+                page.add(
+                    ft.Row(
+                        [
+                            ft.Image(thumb['url'], width=200, height=200),
+                            ft.Text(info['title'],width=200,overflow=ft.TextOverflow.ELLIPSIS, max_lines=4)
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                    ),
+                )
+                page.update()
+            
 
     def download(e):
         try:
             download_video(str(link_input.value))
         except:
-            video_status.value = "Link inválido"
+            status_text.value = "Link inválido"
             page.update()
 
     page.navigation_bar = ft.NavigationBar(
