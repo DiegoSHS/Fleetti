9e9d87d7218a9ae0f403d3de73f8aa6f8a035ee5
Diego López Ariza <diegoawesome2017@outlook.com>
2026-03-26 19:08:25 -0600

    Added new app flow and minor changes

--- main.py
+++ main.py
@@ -1,110 +1,149 @@
+import os.path
 import flet as ft
 from flet import icons
-from yt_dlp import YoutubeDL as YDL
+from yt_dlp import YoutubeDL
+
+class VideoInfo:
+    def __init__(
+            self,
+            video_link: str = '',
+            video_title: str = '',
+            status: str = 'Esperando',
+            thumbnail_url='',
+    ):
+        self.video_link = video_link
+        self.video_title = video_title
+        self.status = status
+        self.thumbnail_url = thumbnail_url
+
+    def reset(self):
+        self.video_link = ''
+        self.video_title = ''
+        self.status = 'Esperando'
+        self.thumbnail_url = ''
+        return self
+
+    def set_thumbnail(self, thumbnails) -> str:
+        for thumbnail in thumbnails:
+            if 'resolution' not in thumbnail:
+                continue
+            if thumbnail['resolution'] != "1920x1080":
+                continue
+            self.thumbnail_url = thumbnail['url']
+        return self.thumbnail_url
 
 def main(page: ft.Page):
-    page.title = "Flet counter example"
+    def auto_update(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            page.update()
+            return result
+
+        return wrapper
+
+    video_info: VideoInfo = VideoInfo()
+    page.title = "Descarga de videos"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    status_text = ft.TextField(value="Aún no has descargado ningún video",read_only=True,border=ft.InputBorder.NONE)
-    progress_bar = ft.ProgressBar(width=400)
+    status_text = ft.TextField(value=video_info.status, read_only=True, border=ft.InputBorder.NONE)
+    progress_bar = ft.ProgressBar(value=100,width=400,opacity=0.5)
     status_bar = ft.Row(
-                [
-                    progress_bar,
-                ],
-                alignment=ft.MainAxisAlignment.CENTER,
-            )
+        [
+            progress_bar,
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+    )
     video_status = ft.Row(
         [
             status_text,
         ],
         alignment=ft.MainAxisAlignment.CENTER,
     )
-    
-    link_input = ft.TextField(label="Escribe el link de tu video", width=200)
-    
-    
+
+    @auto_update
+    def get_video_info(url:str):
+        status_text.value = 'Buscando video'
+        page.update()
+        try:
+            with YoutubeDL() as ydl:
+                info = ydl.extract_info(url,download=False)
+                thumbnails = info.get('thumbnails', [])
+                thumbnail_url = video_info.set_thumbnail(thumbnails)
+                add_thumbnail(thumbnail_url, info['title'])
+                status_text.value = 'Video encontrado'
+        except Exception:
+            status_text.value = 'Error al buscar video'
+            video_info.thumbnail_url = ''
+
+    link_input = ft.TextField(label="Video link", width=200,on_change=lambda event: get_video_info(link_input.value))
+
     def get_directory_path(e):
         download(download_path=e.path)
-         
+
     filepicker = ft.FilePicker(on_result=get_directory_path)
-    
+
+    @auto_update
     def progress_hook(d):
-        page.add(
-            status_bar
-        )
         if d['status'] == 'finished':
-            status_text.value = "Video descargado"
-            page.update()
+            status_text.value = 'Finalizó la descarga'
+            progress_bar.value= 100
         else:
-            progress_bar.value = float(d['_percent_str'].replace('%','')) * 0.01
-            status_text.value = f"Descargando... {d['_percent_str']} a {d['_speed_str']}"
-            page.update()
-        page.controls.pop()
-    
-    
-    global added_thumbail    
-    added_thumbail = False
-    
-    def add_thumbnail(thumb, title):
-        global added_thumbail
-        if added_thumbail:
+            progress_bar.value = float(d['_percent_str'].replace('%', '')) * 0.01
+            status_text.value =f"Descargado: {d['_percent_str']} Velocidad: {d['_speed_str']}"
+
+    @auto_update
+    def add_thumbnail(thumbnail_url:str, title:str):
+        if video_info.thumbnail_url:
             page.controls.pop()
         page.add(
-                    ft.Row(
-                        [
-                            ft.Image(thumb, width=200,border_radius=ft.border_radius.all(10)),
-                            ft.Text(title,width=200,overflow=ft.TextOverflow.ELLIPSIS, max_lines=4)
-                        ],
-                        alignment=ft.MainAxisAlignment.CENTER,
-                    ),
-                )
-        page.update()
-        if not added_thumbail:
-            added_thumbail = True
-            
-    def download_video(url,path):
-        with YDL({
-                'outtmpl': fr"{path}\%(title)s.%(ext)s",
-            }) as ydl:
+            ft.Row(
+                [
+                    ft.Image(thumbnail_url, width=200, border_radius=ft.border_radius.all(10)),
+                    ft.Text(title, width=200, overflow=ft.TextOverflow.ELLIPSIS, max_lines=4)
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            ),
+        )
+
+    def download_video(url, path):
+        with YoutubeDL({
+            'outtmpl': fr"{path}\%(title)s.%(ext)s",
+        }) as ydl:
             ydl.add_progress_hook(progress_hook)
-            info = ydl.extract_info(url,download=False)
+            info = ydl.extract_info(url, download=False)
             filename = ydl.prepare_filename(info)
             filepath = ydl.get_output_path(filename=filename)
-            if ydl._ensure_dir_exists(filepath):
-                status_text.value = "Video ya descargado"
+            if os.path.exists(filepath):
+                status_text.value = 'Video descargado'
             ydl.build_format_selector(format_spec='bestvideo+bestaudio/best')
             ydl.download(url_list=[url])
-            thumbails = info.get('thumbnails', [])
-            for thumb in thumbails:
-                if 'resolution' not in thumb:
-                    continue
-                if thumb['resolution'] != "1920x1080":
-                    continue
-                add_thumbnail(thumb['url'],info['title'])
-                
-            
 
+    @auto_update
     def download(download_path):
+        page.add(
+            ft.Row(
+                [
+                    status_bar
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            ),
+        )
         try:
-            download_video(str(link_input.value),download_path)
-        except Exception as e:
-            print(e)
-            status_text.value = "Link inválido"
-            page.update()
-
+            download_video(str(link_input.value), download_path)
+        except Exception:
+            status_text.value = "Error al descargar"
+            video_info.thumbnail_url = ''
 
     page.navigation_bar = ft.NavigationBar(
         destinations=[
             ft.NavigationDestination(
-            icon=icons.EXPLORE, label="Explore"
-        ),
-        ft.NavigationDestination(
-            icon=icons.BOOKMARK, label="Saved"
-        ),
+                icon=icons.EXPLORE, label="Explore"
+            ),
+            ft.NavigationDestination(
+                icon=icons.BOOKMARK, label="Saved"
+            ),
         ]
     )
-    
-    
+
     page.overlay.extend([filepicker])
     page.add(
         ft.Row(
@@ -120,6 +159,10 @@ def main(page: ft.Page):
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         ),
+        ft.Row(
+            [   ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
     )
 
 
